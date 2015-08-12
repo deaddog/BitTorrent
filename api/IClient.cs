d358f431df16dd3f419e19ba3bfcb6a52ada169a
deaddog <mikkelslsl@gmail.com>
2015-08-12 13:11:25 +0200

    Created an interface for implementing bittorrent APIs.

---
+++ api/IClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BitTorrent.API
+{
+    public interface IClient
+    {
+        Task<bool> AddFromURL(string url, string downloadPath = null);
+        Task<bool> AddFromTorrentFile(string filepath, string downloadPath = null);
+
+        Task<bool> RemoveTorrent(InfoHash hash);
+        Task<TorrentInfo[]> ListTorrents();
+
+        Task<bool> Move(InfoHash hash, string newpath);
+
+        Task<bool> SetState(IEnumerable<InfoHash> torrents, ActiveStates state);
+        Task<bool> SetStateAll(ActiveStates state);
+
+        Task<bool> SetLabels(IEnumerable<InfoHash> torrents, string[] labels);
+        Task<bool> SetLabelsAll(string[] labels);
+
+        Task<bool> SetPriority(IEnumerable<InfoHash> torrents, int priority);
+        Task<bool> SetPriorityAll(int priority);
+    }
+}
