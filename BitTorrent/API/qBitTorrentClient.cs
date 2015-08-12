5cfdeea73b67ba2dfaec9f08f78e28d67fc49dc8
deaddog <mikkelslsl@gmail.com>
2015-08-12 22:13:28 +0200

    Created a nested type for implementing the RequestHandler used by the qBitTorrent client.

--- BitTorrent/API/qBitTorrentClient.cs
+++ BitTorrent/API/qBitTorrentClient.cs
@@ -1,11 +1,42 @@
-﻿using System;
+﻿using API;
+using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace BitTorrent.API
 {
     public class qBitTorrentClient : IClient
     {
+        #region
+
+        private class qRequestHandler : RequestHandler
+        {
+            private readonly string username;
+            private readonly string password;
+
+            public qRequestHandler(string server, int port, string username, string password)
+                : base($"{ensureHttp(server)}:{port}")
+            {
+                this.username = HttpUtility.UrlEncode(username);
+                this.password = HttpUtility.UrlEncode(password);
+            }
+
+            private static string ensureHttp(string server)
+            {
+                var m = Regex.Match(server, "^(?<protocol>[a-z]+)://");
+                if (!m.Success)
+                    return "http://" + server;
+                else if (m.Groups["server"].Value.ToLower() == "http")
+                    return server;
+                else
+                    throw new ArgumentException("Malformed server url.", nameof(server));
+            }
+        }
+
+        #endregion
+
         public Task<bool> AddFromTorrentFile(string filepath, string downloadPath = null)
         {
             throw new NotImplementedException();
