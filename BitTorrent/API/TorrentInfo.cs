7cbbbc4aa380c6468b5556408ceabce8798db8c5
Stefan Micheelsen <stufkan@gmail.com>
2015-08-15 21:06:37 +0200

    Change TorrentInfo to contain only a single label instead of a collection of labels
    As qBitTorrent does not support multiple labels, the TorrentInfo class is changed
    to only contain a single string instead of a collection of strings

--- BitTorrent/API/TorrentInfo.cs
+++ BitTorrent/API/TorrentInfo.cs
@@ -10,19 +10,19 @@ namespace BitTorrent.API
         public int Priority { get; }
         public ActiveStates ActiveState { get; }
         public DownloadStates DownloadState { get; }
-        public ReadOnlyCollection<string> Labels { get; }
+        public string Label { get; }
         public ulong Size { get; }
         public ulong Remaining { get; }
         public ulong Uploaded { get; }
 
-        public TorrentInfo(InfoHash hash, string name, int priority, ActiveStates activestate, DownloadStates downloadstate, IEnumerable<string> labels, ulong size, ulong remaining, ulong uploaded)
+        public TorrentInfo(InfoHash hash, string name, int priority, ActiveStates activestate, DownloadStates downloadstate, string label, ulong size, ulong remaining, ulong uploaded)
         {
             Hash = hash;
             Name = name;
             Priority = priority;
             ActiveState = activestate;
             DownloadState = downloadstate;
-            Labels = new ReadOnlyCollection<string>(new List<string>(labels));
+            Label = label;
             Size = size;
             Remaining = remaining;
             Uploaded = uploaded;
