2647522d3044181c4880e568156e72e86c794ad1
deaddog <mikkelslsl@gmail.com>
2015-08-21 12:43:10 +0200

    TorrentManager exposes its client internally.

--- BitTorrent/TorrentManager.cs
+++ BitTorrent/TorrentManager.cs
@@ -6,6 +6,7 @@ namespace BitTorrent
     public class TorrentManager
     {
         private IClient client;
+        internal IClient Client => client;
 
         public TorrentManager(IClient client)
         {
