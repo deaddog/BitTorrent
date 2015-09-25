e360dfdb565cbe919445e8b093b55d0e32d54d03
deaddog <mikkelslsl@gmail.com>
2015-09-25 16:27:25 +0200

    Added an update method in TorrentManager that retrieves the list of torrents from the client and updates an internal list of torrents.

--- BitTorrent/TorrentManager.cs
+++ BitTorrent/TorrentManager.cs
@@ -1,5 +1,6 @@
 ﻿using BitTorrent.API;
 using System;
+using System.Collections.Generic;
 
 namespace BitTorrent
 {
@@ -8,12 +9,45 @@ namespace BitTorrent
         private IClient client;
         internal IClient Client => client;
 
+        private List<Torrent> torrents;
+
         public TorrentManager(IClient client)
         {
             if (client == null)
                 throw new ArgumentNullException(nameof(client));
 
             this.client = client;
+            this.torrents = new List<Torrent>();
+        }
+
+        public Torrent[] GetTorrents()
+        {
+            Update();
+            return torrents.ToArray();
+        }
+
+        public void Update()
+        {
+            var info = client.ListTorrents().Result;
+
+            List<Torrent> notUpdated = new List<Torrent>(torrents);
+            foreach (var t in info)
+            {
+                var old = torrents.Find(x => x.Hash.Equals(t.Hash));
+                if (old != null)
+                {
+                    old.ManagerUpdate(t);
+                    notUpdated.Remove(old);
+                }
+                else
+                    torrents.Add(new Torrent(this, t));
+            }
+
+            foreach (var t in notUpdated)
+            {
+                t.ManagerDelete();
+                torrents.Remove(t);
+            }
         }
     }
 }
