fdef527c2532f8ec87e1eb5b8674bef78f9bf3c2
deaddog <mikkelslsl@gmail.com>
2016-01-08 17:53:11 +0100

    Created a class for representing links (local and online) references to torrents.

---
+++ BitTorrent/TorrentLink.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BitTorrent
+{
+    public class TorrentLink
+    {
+        private string path;
+        private LinkType linkType;
+
+        private TorrentLink(string path, LinkType linkType)
+        {
+            if (path == null)
+                throw new ArgumentNullException(nameof(path));
+
+            this.path = path;
+            this.linkType = linkType;
+        }
+
+        public static TorrentLink FromURL(string url)
+        {
+            return new TorrentLink(
+                url,
+                url.StartsWith("magnet:?") ? LinkType.Magnet : LinkType.OnlineFile);
+        }
+        public static TorrentLink FromLocal(string filepath)
+        {
+            return new TorrentLink(filepath, LinkType.Magnet);
+        }
+
+        internal string Path => path;
+        public LinkType LinkType => linkType;
+    }
+}
